988b8e1ff5bb4baccb21f6ef6600488dba10ab5b
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-09-13 14:31:27 -0300

    refactor(1-weather-app): remove unecessary interfaces

--- 1-weather-app/src/useCases/CreateCity/ICreateCityUseCase.ts
+++
@@ -1,6 +0,0 @@
-import ICreateCityDTO from './CreateCityDTO'
-import CreateCityUseCaseReturn from './CreateCityUseCaseReturn'
-
-export default interface ICreateCityUseCase {
-  createCity(data: ICreateCityDTO): Promise<CreateCityUseCaseReturn>
-}
