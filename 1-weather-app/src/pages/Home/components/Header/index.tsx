3f88ea73b7fd316996b8a45d1713c67cf3abe01a
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-09-14 10:52:21 -0300

    feat(1-weather-app/pages): add header UI in Home

--- 1-weather-app/src/pages/Home/components/Header/index.tsx
+++ 1-weather-app/src/pages/Home/components/Header/index.tsx
@@ -1,6 +1,18 @@
 import React from 'react'
-import { HeaderContainer } from './styles'
+import {
+  HeaderContainer,
+  HeaderContent,
+  HeaderSubtitle,
+  HeaderTitle,
+} from './styles'
 
 export default function Header() {
-  return <HeaderContainer></HeaderContainer>
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <HeaderTitle>Simple Weather App</HeaderTitle>
+        <HeaderSubtitle>I hope it's a sunny day</HeaderSubtitle>
+      </HeaderContent>
+    </HeaderContainer>
+  )
 }
--- 1-weather-app/src/pages/Home/components/Header/styles.ts
+++ 1-weather-app/src/pages/Home/components/Header/styles.ts
@@ -1,5 +1,47 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 export const HeaderContainer = styled.div`
-  
-`;
+  display: flex;
+  flex-direction: column;
+  background-color: var(--color-primary);
+
+  justify-content: center;
+
+  @media (min-width: 800px) {
+    height: 260px;
+  }
+`
+
+export const HeaderContent = styled.div`
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  color: var(--color-text-primary);
+  padding: 1.6rem 0;
+  align-self: flex-start;
+
+  @media (min-width: 800px) {
+    flex: 1;
+
+    width: 100%;
+    max-width: 800px;
+
+    margin: 0 auto;
+    padding-bottom: 48px;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+  }
+`
+
+export const HeaderTitle = styled.h1`
+  font-weight: 700;
+  font-size: 3.6rem;
+`
+
+export const HeaderSubtitle = styled.h3`
+  font-weight: 500;
+  font-size: 1.8rem;
+`
