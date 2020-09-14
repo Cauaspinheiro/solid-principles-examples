943d4c8cb879585359f3721e8742ecabd977a164
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-09-13 22:04:37 -0300

    feat(1-weather-app/res): add localStorage implementation for cities res

---
+++ 1-weather-app/src/repositories/CitiesRepository/implementations/LocalStorageRepository.ts
@@ -0,0 +1,48 @@
+import City from '../../../entities/City'
+import ICitiesRepository from '../ICitiesRepository'
+
+export interface ILocalStorageRepositorySchema {
+  name: string
+}
+
+export const REPOSITORY_KEY = 'citiesUsed'
+
+export default class LocalStorageRepository implements ICitiesRepository {
+  async findByName(cityName: string) {
+    const storageValue = localStorage.getItem(REPOSITORY_KEY)
+
+    if (!storageValue) return null
+
+    const cities: ILocalStorageRepositorySchema[] = JSON.parse(storageValue)
+
+    const city = cities.find((value) => {
+      if (value.name.toLowerCase() === cityName.toLowerCase()) return true
+
+      return false
+    })
+
+    console.log(city)
+
+    if (!city) return null
+
+    return city
+  }
+
+  async save(city: City) {
+    const { name } = city
+
+    const storageValue = localStorage.getItem(REPOSITORY_KEY)
+
+    let cities: ILocalStorageRepositorySchema[]
+
+    if (!storageValue) {
+      cities = [{ name }]
+    } else {
+      cities = JSON.parse(storageValue)
+
+      cities = [...cities, { name }]
+    }
+
+    localStorage.setItem(REPOSITORY_KEY, JSON.stringify(cities))
+  }
+}
